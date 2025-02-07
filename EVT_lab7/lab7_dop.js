92c419b816cb7698db0327e89839afb3870c71ed
sombrecloud18 <154019334+sombrecloud18@users.noreply.github.com>
2025-02-07 13:00:05 +0300

    Add files via upload

---
+++ EVT_lab7/lab7_dop.js
@@ -0,0 +1,11 @@
+function setFlexDirection(direction) {
+    document.querySelector('.container').style.flexDirection = direction;
+}
+
+function setFlexWrap(wrap) {
+    document.querySelector('.container_two').style.flexWrap = wrap;
+}
+
+function setJustifyContent(align) {
+    document.querySelector('.container_three').style.justifyContent = align;
+}
