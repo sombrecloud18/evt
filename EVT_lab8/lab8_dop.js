a65f87aa551f46e94131c0ed5e887f4fc660855f
sombrecloud18 <154019334+sombrecloud18@users.noreply.github.com>
2025-02-11 20:58:54 +0300

    Add files via upload

---
+++ EVT_lab8/lab8_dop.js
@@ -0,0 +1,11 @@
+function setDisplay(type) {
+    document.querySelector('.container').style.display = type;
+}
+
+function setTemplateColumns(type) {
+    document.querySelector('.container_two').style.gridTemplateColumns = type;
+}
+
+function setTemplateRows(type) {
+    document.querySelector('.container_three').style.gridTemplateRows = type;
+}
